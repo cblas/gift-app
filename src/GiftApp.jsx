35704090a066aedb623d14c31b757259d2c1b3f0
cblas <cblas@DESKTOP-AVAFUTS>
2023-08-24 21:27:48 -0600

    functionality add category name and category list

--- src/GiftApp.jsx
+++ src/GiftApp.jsx
@@ -1,6 +1,28 @@
+import { useState } from "react"
 
 export const GiftApp = () => {
+  const [categories, setCategories] = useState(['Jose', 'Emi']);
+  const [name, setName] = useState('');
+
+  const handleInputName = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleAddName = () => {
+    setCategories([...categories, name]);
+  };
+
   return (
-    <div>GiftApp</div>
+    <>
+        <h2>Gift App</h2>
+        <input type="text" onChange={handleInputName}/>
+        <button onClick={handleAddName}>Add name</button>
+        <ol>
+            {categories.map((category) => {
+                return <li key={category}>{category}</li>
+            })}
+        </ol>
+       
+    </>
   )
 }
